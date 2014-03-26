6905b3ee05c9145994e166437e6d1a49fdef1974
Fisica2014-FF <directoryh22@gmail.com>
2014-03-26 19:58:18 +0100

    Primo commit, aggiunta natura c++ e i dati formattati

---
+++ src/MainEstensimetro.cpp
@@ -0,0 +1,14 @@
+/*
+ * MainEstensimetro.cpp
+ *
+ *  Created on: Mar 26, 2014
+ *      Author: francesco
+ */
+
+
+#include <iostream>
+
+int main(int numparam, char* args[]) {
+	using namespace std;
+	cout << "Ciao";
+}
